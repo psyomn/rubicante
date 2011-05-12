e31277d17740c2ee589c2fbd9bfe5cd8cce907cc
psyomn <psyomn@xenagoras.net>
2011-05-12 15:45:51 -0400

    changing to the appropriate channel.

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require_relative 'irc.class.rb'
 
-botty = Bot.new('irc.freenode.net', 6667, '#shameful', 'rubicante')
+botty = Bot.new('irc.freenode.net', 6667, '#spaceconcordia', 'rubicante')
 
 botty.start
 
